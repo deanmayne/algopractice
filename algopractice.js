ce546d383ca3e9e6d1841063aa4d7cf3288b52c3
Dean Mayne <deancmayne@gmail.com>
2021-02-22 21:15:44 -0800

    feb 22nd

--- algopractice.js
+++ algopractice.js
@@ -151,3 +151,20 @@ function minStart(arr) {
 }
 
 console.log(minStart(arr));
+
+let bubbleSort = (inputArr) => {
+  let len = inputArr.length;
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < len; i++) {
+      if (inputArr[i] > inputArr[i + 1]) {
+        let tmp = inputArr[i];
+        inputArr[i] = inputArr[i + 1];
+        inputArr[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return inputArr;
+};
