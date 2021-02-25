549ec2d2878037b93db161c878b0d21900b4007c
Dean Mayne <deancmayne@gmail.com>
2021-02-24 19:25:22 -0800

    feb 24th

--- algopractice.js
+++ algopractice.js
@@ -168,3 +168,19 @@ let bubbleSort = (inputArr) => {
   } while (swapped);
   return inputArr;
 };
+
+function caesarCipher(str, shift) {
+  let alpha = "abcdefghijklmnopqrstuvwxyz";
+
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (alpha.includes(str[i])) {
+      let newI = (alpha.indexOf(str[i]) + shift) % 26;
+      result.push(alpha[newI]);
+    } else {
+      result.push(str[i]);
+    }
+  }
+
+  return result.join("");
+}
