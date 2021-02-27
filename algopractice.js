9b649bcc5b9077091473ddf9a2fe25def339c2db
Dean Mayne <deancmayne@gmail.com>
2021-02-26 16:15:30 -0800

    feb 26th

--- algopractice.js
+++ algopractice.js
@@ -257,3 +257,16 @@ function finalInstances(instances, averageUtil) {
   }
   return instances;
 }
+
+function binarySearch(arr, target) {
+    if (arr.length === 0) return -1;
+    const mid = Math.floor(arr.length / 2);
+    if (target < arr[mid]) {
+        return binarySearch(arr.slice(0, mid), target);
+    } else if (target > arr[mid]) {
+        const idx = binarySearch(arr.slice(mid + 1), target);
+        return idx === -1 ? -1 : mid + 1 + idx;
+    } else {
+        return mid;
+    }
+}
