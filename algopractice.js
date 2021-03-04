c043bf8103a706a8e632c0bdeeaa3500dcdd0c28
Dean Mayne <deancmayne@gmail.com>
2021-03-03 22:01:25 -0800

    mar 3rd

--- algopractice.js
+++ algopractice.js
@@ -309,3 +309,25 @@ function removeDuplicates(s, k) {
 
   return arr.join("");
 }
+
+function maxCandies(candyType) {
+  let candies = {};
+  let max = candyType.length / 2;
+  candyType.forEach((candy) => {
+    if (candies[candy]) {
+      candies[candy] += 1;
+    } else {
+      candies[candy] = 1;
+    }
+  });
+  let numTypes = Object.values(candies).length;
+  console.log("func", max, numTypes);
+  if (numTypes > max) {
+    return max;
+  } else {
+    return numTypes;
+  }
+  // return numTypes < max ? numTypes : max
+}
+
+console.log(maxCandies([1, 1, 2, 2, 3, 3]));
