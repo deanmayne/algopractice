9ac7f845ed50c48b272a64c91f0873888ba5f7af
Dean Mayne <deancmayne@gmail.com>
2021-03-10 08:23:47 -0800

    mar 10

--- algopractice.js
+++ algopractice.js
@@ -452,3 +452,30 @@ function reverseLinkedList(linkedlist) {
   return linkedlist;
 }
 
+function minSetSize(arr) {
+    let targetLength = arr.length / 2;
+    let count = 0;
+    let accumCount = 0;
+    let obj = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!obj[arr[i]]) {
+            obj[arr[i]] = 1;
+        } else {
+            obj[arr[i]] += 1;
+        }
+    }
+
+    let valuesArray = Object.values(obj).sort((a, b) => b - a);
+
+    for (let i = 0; i < valuesArray.length; i++) {
+        count += 1;
+        accumCount += valuesArray[i];
+
+        if (accumCount >= targetLength) {
+            return count;
+        }
+    }
+
+    return 0;
+};
